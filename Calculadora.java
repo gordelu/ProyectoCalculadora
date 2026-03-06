82f91d0d53c506a5184ea025af1c503b5e18150d
gordelu <40674196+gordelu@users.noreply.github.com>
2026-03-06 18:18:11 +0100

    Commit Inicial - Clase Calculadora con metodo suma

---
+++ Calculadora.java
@@ -0,0 +1,26 @@
+public class Calculadora {
+
+    private int num1;
+    private int num2;
+
+    // Constructor
+    public Calculadora(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    // Método suma
+    public int suma() {
+        return num1 + num2;
+    }
+
+    // Método realizarOperacion
+    public int realizarOperacion(String operacion) {
+
+        if (operacion.equals("suma")) {
+            return suma();
+        }
+
+        return 0;
+    }
+}
